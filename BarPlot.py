250fc99a819e1af83c8124a4907a7581ca5ea292
Eyebag69 <singhaashish4522@gmail.com>
2026-01-14 00:08:27 +0530

    Bar Plot

---
+++ BarPlot.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+data = {
+  "Name":["Aashish","Piyush","Raj","Aayush","Gunjita","Kartikeya","Sneha","Golu","Aditya","Rohit","Priyanshu"],
+  "Marks":[85,89,90,91,80,81,95,93,78,82,98],
+  "Age":[20,20,19,18,21,18,22,21,20,19,20]
+}
+df = pd.DataFrame(data)
+
+plt.bar(df["Name"], df["Marks"])
+plt.title("Marks Distribution")
+plt.xlabel("Name")
+plt.ylabel("Marks")
+plt.show()
