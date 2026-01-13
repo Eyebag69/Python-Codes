d7359bdc2107f12f2e9fd19f8b1b9ebd73e36f04
Eyebag69 <singhaashish4522@gmail.com>
2026-01-13 23:56:32 +0530

    Pandas stats

---
+++ PandaStats.py
@@ -0,0 +1,12 @@
+import pandas as pd
+data = {
+  "Name":["Aashish","Piyush","Raj","Aayush","Gunjita","Kartikeya","Sneha","Golu","Aditya","Rohit","Priyanshu"],
+  "Marks":[85,89,90,91,80,81,95,93,78,82,98],
+  "Age":[20,20,19,18,21,18,22,21,20,19,20]
+}
+df = pd.DataFrame(data)
+print(df["Marks"].max())
+print(df["Marks"].min())
+print(df["Marks"].count())
+print(df["Marks"].median())
+print(df["Marks"].mode())
